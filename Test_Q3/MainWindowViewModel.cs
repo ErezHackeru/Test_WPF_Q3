ce027382697ec1c0ff226a1807c7d760e5a643b6
ErezHackeru <45459156+ErezHackeru@users.noreply.github.com>
2019-08-28 21:36:15 +0300

    Add files via upload

---
+++ Test_Q3/MainWindowViewModel.cs
@@ -0,0 +1,63 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test_Q3
+{
+    public class MainWindowViewModel
+    {
+        public DelegateCommand MyDelegate { get; set; }
+        private bool IsProcessOver;
+        public Results Results { get; set; }        
+        
+        public string Url { get; set; }
+
+        public MainWindowViewModel()
+        {
+            MyDelegate = new DelegateCommand(ExecuteCommand, CanExecuteMethod);
+            IsProcessOver = true;
+            Results = new Results() { Result= "Please wait..." };
+            
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    MyDelegate.RaiseCanExecuteChanged(); // go check the enable/disable
+                    Thread.Sleep(500);
+                }
+
+            });
+        }
+
+        // =========================== delegate
+        private bool CanExecuteMethod()
+        {
+            return IsProcessOver;
+        }
+
+        private async void ExecuteCommand()
+        {
+            IsProcessOver = false;
+            string text;
+            await Task.Run(() =>
+            {
+                using (WebClient client = new WebClient())
+                {
+                    text = client.DownloadString(Url);
+                }
+
+                int size = System.Text.ASCIIEncoding.ASCII.GetByteCount(text);
+
+                Results.Result = $"The size is: {size} bytes";
+            });          
+            IsProcessOver = true;
+        }
+    }
+}
---
+++ Test_Q3/Results.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Q3
+{
+    public class Results : INotifyPropertyChanged
+    {
+        private string result;
+        public string Result
+        {
+            get
+            {
+                return this.result;
+            }
+            set
+            {
+                this.result = value;
+                OnPropertyChanged("Result");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public override string ToString()
+        {
+            return $"Result name {Result}";
+        }
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
